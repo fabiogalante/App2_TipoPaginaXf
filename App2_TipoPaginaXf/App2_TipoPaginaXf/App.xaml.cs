1b115e2e68359f4759c91f11cbc812dda9de6842
Fabio Galante Mans <fabiogalantemans@gmail.com>
2020-05-15 21:57:29 -0300

    Add project files.

---
+++ App2_TipoPaginaXf/App2_TipoPaginaXf/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using App2_TipoPaginaXf.TipoPagina.Carousel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2_TipoPaginaXf
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new TipoPagina3();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
